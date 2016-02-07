8550401ef8a0356ad76797c2553e540e0763b53f
jcastroe1000 <jcastroe1000@alumno.ipn.mx>
2016-02-07 00:31:05 -0600

    erasing line to hide the form wend the mail have been send

--- js/mail.js
+++ js/mail.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
                 success: function(data){
                     $("#ok").html(data);
                     $("#ok").show();
-                    $("#formid").hide();
+                 
                 }
             });
         }
