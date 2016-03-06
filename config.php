15d92ca250b9ec124b1c246a4af75f08702a4cc9
jcastroe1000 <jcastroe1000@alumno.ipn.mx>
2016-03-06 16:42:06 -0600

    change values to conect the db

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
-$mysqli = new mysqli("localhost", "root", "", "sistem_galery");
+$mysqli = new mysqli("localhost", "root", "", "galery_twf");
     if ($mysqli->connect_errno) {
         echo "Failed to connect to MySQL: " . $mysqli->connect_error;   
     }
-    $mysqli2 = new mysqli("localhost", "root", "", "sistem_galery");
+    $mysqli2 = new mysqli("localhost", "root", "", "galery_twf");
     if ($mysqli->connect_errno) {
         echo "Failed to connect to MySQL: " . $mysqli->connect_error;   
     }
