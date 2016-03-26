9cefd09f63cad229525ec2c796e118a8d1d1f9af
jcastroe1000 <jcastroe1000@alumno.ipn.mx>
2016-03-25 18:10:55 -0600

    edit user database

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
-$mysqli = new mysqli("localhost", "root", "", "galery_twf");
+$mysqli = new mysqli("mysql.hostinger.mx", "u183599560_root", "2011301308", "u183599560_twfg");
     if ($mysqli->connect_errno) {
         echo "Failed to connect to MySQL: " . $mysqli->connect_error;   
     }
-    $mysqli2 = new mysqli("localhost", "root", "", "galery_twf");
+    $mysqli2 = new mysqli("mysql.hostinger.mx", "u183599560_root", "2011301308", "u183599560_twfg");
     if ($mysqli2->connect_errno) {
         echo "Failed to connect to MySQL: " . $mysqli2->connect_error;   
     }
-    $mysqli3 = new mysqli("localhost", "root", "", "galery_twf");
+    $mysqli3 = new mysqli("mysql.hostinger.mx", "u183599560_root", "2011301308", "u183599560_twfg");
     if ($mysqli3->connect_errno) {
         echo "Failed to connect to MySQL: " . $mysqli3->connect_error;   
     }
