42dade4580cccd9d0acf376748630aac36cc2f95
jcastroe1000 <jcastroe1000@alumno.ipn.mx>
2016-03-26 14:10:41 -0600

    adding user for production server

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
-$mysqli = new mysqli("mysql.hostinger.mx", "u183599560_root", "2011301308", "u183599560_twfg");
+$mysqli = new mysqli("localhost", "wedding_facroty", "wedding_facroty", "twf_gallery");
     if ($mysqli->connect_errno) {
         echo "Failed to connect to MySQL: " . $mysqli->connect_error;   
     }
-    $mysqli2 = new mysqli("mysql.hostinger.mx", "u183599560_root", "2011301308", "u183599560_twfg");
+    $mysqli2 = new mysqli("localhost", "wedding_facroty", "wedding_facroty", "twf_gallery");
     if ($mysqli2->connect_errno) {
         echo "Failed to connect to MySQL: " . $mysqli2->connect_error;   
     }
-    $mysqli3 = new mysqli("mysql.hostinger.mx", "u183599560_root", "2011301308", "u183599560_twfg");
+    $mysqli3 = new mysqli("localhost", "wedding_facroty", "wedding_facroty", "twf_gallery");
     if ($mysqli3->connect_errno) {
         echo "Failed to connect to MySQL: " . $mysqli3->connect_error;   
     }
