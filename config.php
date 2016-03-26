42594de71dcaadd8cfbbad2df8947c65b8ab4d88
jcastroe1000 <jcastroe1000@alumno.ipn.mx>
2016-03-26 14:40:16 -0600

    correcting typo

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
-$mysqli = new mysqli("localhost", "wedding_facroty", "wedding_facroty", "twf_gallery");
+$mysqli = new mysqli("localhost", "wedding_factory", "wedding_factory", "twf_gallery");
     if ($mysqli->connect_errno) {
         echo "Failed to connect to MySQL: " . $mysqli->connect_error;   
     }
-    $mysqli2 = new mysqli("localhost", "wedding_facroty", "wedding_facroty", "twf_gallery");
+    $mysqli2 = new mysqli("localhost", "wedding_factory", "wedding_factory", "twf_gallery");
     if ($mysqli2->connect_errno) {
         echo "Failed to connect to MySQL: " . $mysqli2->connect_error;   
     }
-    $mysqli3 = new mysqli("localhost", "wedding_facroty", "wedding_facroty", "twf_gallery");
+    $mysqli3 = new mysqli("localhost", "wedding_factory", "wedding_factory", "twf_gallery");
     if ($mysqli3->connect_errno) {
         echo "Failed to connect to MySQL: " . $mysqli3->connect_error;   
     }
