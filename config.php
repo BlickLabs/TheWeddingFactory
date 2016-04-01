3bdfa5f7fd54cd98e9ffd7229a3f837b25479737
jcastroe1000 <jcastroe1000@alumno.ipn.mx>
2016-04-01 00:31:12 -0600

    rewriting databases user name

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
-$mysqli = new mysqli("localhost", "the_wedding_fact", "weddingfactory", "wedding_factory");
+$mysqli = new mysqli("localhost", "wedding_fact", "weddingfactory", "wedding_factory");
     if ($mysqli->connect_errno) {
         echo "Failed to connect to MySQL: " . $mysqli->connect_error;   
     }
-    $mysqli2 = new mysqli("localhost", "the_wedding_fact", "weddingfactory", "wedding_factory");
+    $mysqli2 = new mysqli("localhost", "wedding_fact", "weddingfactory", "wedding_factory");
     if ($mysqli2->connect_errno) {
         echo "Failed to connect to MySQL: " . $mysqli2->connect_error;   
     }
-    $mysqli3 = new mysqli("localhost", "the_wedding_fact", "weddingfactory", "wedding_factory");
+    $mysqli3 = new mysqli("localhost", "wedding_fact", "weddingfactory", "wedding_factory");
     if ($mysqli3->connect_errno) {
         echo "Failed to connect to MySQL: " . $mysqli3->connect_error;   
     }
