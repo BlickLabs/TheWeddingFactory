3faee9e7b52fbd1f0025e94f4cb720021dc6c611
jcastroe1000 <jcastroe1000@alumno.ipn.mx>
2016-04-25 19:24:56 -0500

    adding key and mail to the production

--- model/contacto_mail.php
+++ model/contacto_mail.php
@@ -12,8 +12,8 @@ $asunto = 'Un cliente requiere de mayor información: ';
 
 /* Desarrollo */
 $api_key = 'key-eb656047b090ea091ef7c5d2fbd83dc5';
-$api_domain = 'sandbox3bfa1334fbee4dcca5b08a9b34b46337.mailgun.org';
-$send_to = 'desarrolloblick@gmail.com';
+$api_domain = 'mg.weddingfactory.mx';
+$send_to = 'info@weddingfactory.mx';
 
 
 $name = $name2;
